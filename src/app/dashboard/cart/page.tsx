8462170ef8aa92466fc0b20025597614c038dc1c
alejasalqs <alejandrosalgueroq@hotmail.com>
2024-11-14 20:40:56 -0600

    Show price and taxes based on cookies

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -1,3 +1,4 @@
+import WidgetItem from '@/components/WidgetItem';
 import { Product, products } from '@/products/data/products.data'
 import { ItemCard } from '@/shopping-cart';
 import { cookies } from 'next/headers'
@@ -29,6 +30,7 @@ const CartPage = () => {
     const cart = JSON.parse(cookiesStore.get('cart')?.value ?? '{}') as { [id: string]: number }
     console.log(cart)
     const products = getProductsInCart(cart)
+    const totalToPay = products.reduce((prevVal, currentVal) => (currentVal.product.price * currentVal.quantity)  + prevVal, 0)
 
     return (
         <div>
@@ -39,6 +41,14 @@ const CartPage = () => {
                     {
                         products.map(({product, quantity}) => (<ItemCard key={product.id} product={product} quantity={quantity}/>))
                     }
+                </div>
+                <div className='flex flex-col w-full sm:w-4/12'>
+                    <WidgetItem title='Total a pagar'>
+                        <div className='mt2 flex justify-center gap-4'>
+                            <h3 className='text-3xl text-gray-700 font-bold'>Total: ${(totalToPay * 1.15).toFixed(2)}</h3>
+                        </div>
+                        <span className='font-bold text-center text-gray-500'>Impuestos: 15% ${(totalToPay * 0.15).toFixed(2)}</span>
+                    </WidgetItem>
                 </div>
             </div>
         </div>
