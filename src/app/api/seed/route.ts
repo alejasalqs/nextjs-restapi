8147eb343ed6e950c9ba058c31a32670a0b2b55d
alejasalqs <alejandrosalgueroq@hotmail.com>
2024-11-26 13:19:44 -0600

    Update todos request by userId

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,22 +1,34 @@
 import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import bcrypt from 'bcryptjs'
 
 export async function GET(request: Request) { 
+   
     await prisma.todo.deleteMany(); // delete * from todo
+    await prisma.user.deleteMany(); // delete * from user
 
-    const todo = await prisma.todo.createMany({
-        data: [{
-            description: "Piedra del alma", complete: true
-        },{
-            description: "Piedra del poder"
-        },{
-            description: "Piedra del espacio"
-        },{
-            description: "Piedra del realidad"
-        }]
+    const user = await prisma.user.create({
+        data: {
+            email: 'test1@test.com',
+            password: bcrypt.hashSync('test'),
+            name: 'Test User',
+            roles: ['admin','client'],
+            todos: {
+                create: [{
+                    description: "Piedra del alma", complete: true
+                },{
+                    description: "Piedra del poder"
+                },{
+                    description: "Piedra del espacio"
+                },{
+                    description: "Piedra del realidad"
+                }]
+            }
+        }
     })
 
   return NextResponse.json({
-    message: "seed executed"
+    message: "seed executed",
+    user
   })
 }
--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,11 +1,14 @@
 export const dynamic = 'force-dynamic' // fuerza una revalidacion de data
 export const revalidate = 0 // se asegura que siempre sea dinamicamente generada
 
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/lib/prisma";
 import { NewTodo, TodosGrid } from "@/todos";
+import { getServerSession } from "next-auth";
 
 export default async function RestTodosPage() {
-  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
+  const session = await getServerSession(authOptions)
+  const todos = await prisma.todo.findMany({ where: { userId: session?.user?.id }, orderBy: { description: 'asc' } })
   return (
     <div>
       <div className="w-full px-3 mx-5 mb-5">
